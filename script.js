e6caf6ae303fa4d580bfed843d9103dd1c2a4141
syedabdulbasitali12 <https://github.com/syedabdulbasitali12/even-odd.git>
2022-08-19 22:10:46 +0500

    'fixed'

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const listItems = []
 
+
+const addItem = (e) => {
+    e.preventDefault();
+    console.log('fun')
+
+    const toDoItem1 = document.querySelector('#toDoItem').value;
+    console.log(toDoItem1)
+    
+    listItems.push = 'hello'
+
+}
+// console.log(listItems)
