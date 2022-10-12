1d20d87046b8cba52d2dbba78827f6f45f7fd094
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-12 15:44:04 +0500

    push

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
-const listItems = []
-
+const listItems = [];
 
 const addItem = (e) => {
-    e.preventDefault();
-    console.log('fun')
+  e.preventDefault();
 
-    const toDoItem1 = document.querySelector('#toDoItem').value;
-    console.log(toDoItem1)
-    
-    listItems.push = 'hello'
+  const toDoItem = document.querySelector("#toDoItem").value;
+  //   console.log(toDoItem1);
+  //   listItems.push(toDoItem1);
+  //   console.log(listItems);
+  axios
+    .post(`https://quaint-scrubs-cow.cyclic.app/todo`, {
+      text: toDoItem,
+    })
+    .then((response) => {
+      console.log(response.data);
 
-}
-// console.log(listItems)
+      document.querySelector("#todoList").innerHTML += "";
+      response.data.data.map((eachToDo) => {
+        document.querySelector("#todoList").innerHTML += eachToDo;
+        document.querySelector("#todoList").innerHTML += "<br>";
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+// addItem();
+
+
+
+
+    axios
+      .get(`https://quaint-scrubs-cow.cyclic.app/todos`)
+      .then((response) => {
+        console.log(response.data);
+  
+        document.querySelector("#todoList").innerHTML += "";
+        response.data.data.map((eachToDo) => {
+          document.querySelector("#todoList").innerHTML += eachToDo;
+          document.querySelector("#todoList").innerHTML += "<br>";
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+
+
+
+
+
+
+
+
+// const myGetDataFunction = async () => {
+//   await fetch(
+//     `https://api.weatherapi.com/v1/current.json?key=25175e31b7074cfc895204529222906&q=${city}`
+//   )
+//     .then((response) => response.json())
+//     .then((json) => {
+//       console.log(json);
+//       // document.querySelector('#userName').innerHTML = `My name is ${json?.name}`
+//     })
+//     .catch((reject) => console.log(reject));
+// };
