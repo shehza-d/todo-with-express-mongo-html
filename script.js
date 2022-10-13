5496ef32737d273b275e1b830907815ecbc0ce07
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-13 22:45:37 +0500

    update

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 // mongoDB server ma install hoga ya clint js ma
 //error status 500
 
-const API_KEY = 'https://gray-exuberant-nightingale.cyclic.app'
-
+const API_KEY = "https://gray-exuberant-nightingale.cyclic.app";
 
 const addItem = (e) => {
   e.preventDefault();
-
   const toDoItem = document.querySelector("#toDoItem").value;
+  toDoItem.trim();
+  if (toDoItem.length > 30) {
+    alert("string can't be greater then 30");
+    return;
+  }
+  if (toDoItem === "") return;
   //   console.log(toDoItem1);
   axios
     .post(`${API_KEY}/todo`, {
       text: toDoItem,
     })
     .then((response) => {
-      console.log(response.data);
+      console.log(response.data.message);
       refreshList();
-
       // document.querySelector("#messagesss").innerHTML = response.data.message;
       // setTimeout(() => {
       //   document.querySelector("#messagesss").innerHTML = "";
       // }, 2000);
-
-
       // document.querySelector("#todoList").innerHTML += "";
       // response.data.data.map((eachToDo) => {
       //   document.querySelector("#todoList").innerHTML += eachToDo;
@@ -40,7 +41,7 @@ const refreshList = () => {
     .then((response) => {
       console.log(response.data);
 
-      document.querySelector("#todoList").innerHTML += "";
+      document.querySelector("#todoList").innerHTML = "";
       response.data.data.map((eachToDo) => {
         document.querySelector("#todoList").innerHTML += eachToDo.text;
         document.querySelector("#todoList").innerHTML += "<br>";
@@ -51,7 +52,10 @@ const refreshList = () => {
     });
 };
 refreshList();
-setInterval(refreshList, 6000);
+
+const deleteToDoList = () => {};
+
+// setInterval(refreshList, 6000);
 // const myGetDataFunction = async () => {
 //   await fetch(
 //     `https://api.weatherapi.com/v1/current.json?key=25175e31b7074cfc895204529222906&q=${city}`
