c2fa2828fb221c53b97b7fe47cfafb955c64b570
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-10-15 19:46:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,16 @@
 //error status 500
 
 //most optimize date funtion
-// (() => {
-// 	document.querySelector("#date").textContent = `${moment().format("D MMM YYYY")}`;
-// 	let time= moment().format("h:mm:s a")
-// 	const timeDiv = document.querySelector("#time").textContent =  time;		
-// 	setInterval(() => {
-// 		 time = moment().format("h:mm:s a");
-// 		 timeDiv =   time;		
-// 	}, 1000);
-//   })();
+(() => {
+	document.querySelector("#date").textContent = `${moment().format("D MMM YYYY")}`;
+	let time= moment().format("h:mm:s a");
+	const timeDiv = document.querySelector("#time");
+	timeDiv.textContent = time;		
+	setInterval(() => {
+		time = moment().format("h:mm:s a");
+		timeDiv.textContent = time;		
+	}, 1000);
+  })();
 
 
 
